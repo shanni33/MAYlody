f8b3db9e60d5b3c4296529c5db98a9df1c64dd88
shanni <sunny3030730@gmail.com>
2021-03-23 23:48:16 +0800

    add TicketCard component

--- src/views/ConcertCard.vue
+++ src/views/ConcertCard.vue
@@ -28,49 +28,12 @@
         class="row justify-content-center my-2 tickets"
         v-if="filterData.length"
       >
-        <div v-for="(data, index) in slicedData" :key="index">
-          <div class="ticket">
-            <label>
-              <header>
-                <span style="color: #2c3e50">{{ data.event }}</span>
-              </header>
-              <div class="top">
-                <div class="row my-3 justify-content-center align-items-center">
-                  <i class="material-icons mx-2">event</i
-                  ><span class="date" style="color: #2c3e50">{{
-                    data.date
-                  }}</span>
-                </div>
-                <!-- <p class="series">{{ data.series }}</p> -->
-                <div class="row my-3 justify-content-center align-items-center">
-                  <i class="material-icons mx-2">location_on</i>
-                  <span class="city" style="color: #2c3e50">
-                    {{ data.city }}</span
-                  >
-                </div>
-                <input type="checkbox" :value="data" v-model="selected" />
-                <span class="material-icons check-btn">check_circle</span>
-              </div>
-            </label>
-            <div class="divider"></div>
-            <label class="more text-align-center">
-              <div class="bottom">
-                <div class="detail-btn px-2 py-1">歌單</div>
-                <input class="detail-toggle" type="checkbox" />
-                <div class="detail">
-                  <hr />
-                  <p
-                    v-for="(song, idx) in data.songs"
-                    :key="idx"
-                    style="color: #2c3e50"
-                  >
-                    {{ idx + 1 }}. {{ song }}
-                  </p>
-                </div>
-              </div>
-            </label>
-          </div>
-        </div>
+        <TicketCard
+          v-for="data in slicedData"
+          :key="data.songs"
+          :inputData="data"
+          :syncSelected.sync="selected"
+        />
       </div>
       <h1 class="warning" v-else>沒有搜尋到東西</h1>
     </div>
@@ -107,10 +70,12 @@
 <script>
 import ConcertDatas from "../assets/dataset.json";
 import SearchBar from "@/components/SearchBar.vue";
+import TicketCard from "@/components/TicketCard.vue";
 
 export default {
   components: {
     SearchBar,
+    TicketCard,
   },
   data() {
     return {
@@ -120,7 +85,7 @@ export default {
       pages: [12, 24, 36, 48, 60],
       perPage: 12,
       searchData: ConcertDatas,
-      selected: JSON.parse(sessionStorage.mySelected || '[]'),
+      selected: JSON.parse(sessionStorage.mySelected || "[]"),
     };
   },
   computed: {
@@ -136,12 +101,13 @@ export default {
           this.slicedData.forEach((data) => {
             // 走訪每筆物件
             if (
-              this.selected.filter((x) => 
-                x.date === data.date &&
-                x.series === data.series &&
-                x.event === data.event &&
-                x.city === data.city &&
-                x.songs.length === data.songs.length
+              this.selected.filter(
+                (x) =>
+                  x.date === data.date &&
+                  x.series === data.series &&
+                  x.event === data.event &&
+                  x.city === data.city &&
+                  x.songs.length === data.songs.length
               ).length == 0
             ) {
               //沒有 才加入集合(重複防呆)
@@ -225,11 +191,10 @@ export default {
   watch: {
     selected: {
       deep: true,
-      handler: function(value) {
+      handler: function (value) {
         sessionStorage.mySelected = JSON.stringify(value);
-      }
-    }
-
+      },
+    },
   },
 };
 </script>
@@ -343,117 +308,6 @@ export default {
   height: 58vh;
   overflow-y: scroll;
 }
-// ticket card
-.ticket {
-  width: 15.5rem;
-  // padding: 0.5rem;
-  background: #eeeeee; // #FFFFF3;
-  border-radius: 5px;
-  margin: 1rem;
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
-}
-.ticket header {
-  background: linear-gradient(60deg, #b99362, #d3b288d0);
-  padding: 0.5em 2em;
-  border-radius: 5px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-  color: #fff;
-}
-.ticket .top,
-.ticket .bottom {
-  padding: 0.5rem;
-  text-align: center;
-  position: relative;
-  cursor: pointer;
-}
-
-.ticket label {
-  display: block;
-  margin: 0;
-}
-
-.date {
-  font-size: 1.1rem;
-}
-.material-icons {
-  color: #b99362;
-  text-shadow: 0 0 2px rgba(139, 139, 139, 0.493);
-}
-
-.ticket input {
-  display: none;
-}
-
-.divider {
-  width: 90%;
-  margin: auto;
-  height: 0.1rem;
-  background: linear-gradient(to right, #222 50%, transparent 50%);
-  background-size: 10px 8px, 100% 1px;
-  // opacity: 0.5;
-}
-
-.ticket .top::before,
-.ticket .top::after {
-  content: "";
-  display: block;
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  background-color: #222;
-  border-radius: 50%;
-  z-index: 1;
-  bottom: 0;
-  margin-bottom: -10px;
-}
-
-.ticket label .top::before {
-  left: 0;
-  margin-left: -10px;
-}
-
-.ticket .top::after {
-  right: 0;
-  margin-right: -10px;
-}
-
-.ticket:hover {
-  transform: scale(1.01);
-}
-
-.check-btn {
-  color: gray;
-}
-
-.ticket input:checked + .check-btn {
-  color: #b99362;
-}
-
-.detail-toggle {
-  display: none;
-}
-
-.detail-toggle:checked + .detail {
-  max-height: none;
-  padding: 0.5rem;
-}
-
-.detail-btn {
-  display: inline-block;
-  background: linear-gradient(60deg, #b99362, #d3b288d0);
-  border-radius: 50px;
-  color: white;
-}
-
-.detail {
-  max-height: 0px;
-  overflow: hidden;
-}
-
 .warning {
   color: white;
 }
