53dea6299ea08f2336deceb810eafe0373b3b367
shanni <sunny3030730@gmail.com>
2021-05-24 23:36:12 +0800

    uninstall 'vue-paginate'

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import Paginate from "vuejs-paginate";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
 Vue.use(BootstrapVue);
-Vue.component("paginate", Paginate);
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
